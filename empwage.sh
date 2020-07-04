7249cf91cb078f939e52ac1b40996c197752ac35
gangadevi <kakarlagangadevi@gmail.com>
2020-07-04 19:54:06 +0530

    total salary for a month calcuated, on number of working days

--- empwage.sh
+++ empwage.sh
@@ -2,19 +2,24 @@
 isPartTime=1
 isFullTime=2
 rateperHour=20;
-empCheck=$((RANDOM%3));
-
-case $empCheck in
-        $isFullTime)
-                empHours=8
-                ;;
-        $isPartTime)
-                empHours=4
-                ;;
-        *)
-        empHours=0
-                ;;
-esac
-empWage=$(( $rateperHour * $empHours ));
-echo $empwage
+numofWorkDays=25;
+totalSalary=0
+for((day=1;day<=numofWorkDays;day++))
+do
+	empCheck=$((RANDOM%3));
+	case $empCheck in
+        	$isFullTime)
+                		empHours=8
+                		;;
+        	$isPartTime)
+                		empHours=4
+                		;;
+        		*)
+        			empHours=0
+                		;;
+	esac
+	dayempWage=$(( $rateperHour * $empHours ));
+	totalSalary=$totalSalary+$dayempwage
+done
+echo $totalSalary
 
